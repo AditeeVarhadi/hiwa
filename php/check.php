3affb8e208596b09b99515af528da17e86123e1a
AditeeVarhadi <50465412+AditeeVarhadi@users.noreply.github.com>
2019-05-12 12:37:36 -0400

    Update check.php

--- php/check.php
+++ php/check.php
@@ -1,14 +1,13 @@
 <?php
 require 'config.phplib';
-
-$msg="";
-if (!array_key_exists('hiwa-user', $_COOKIE) ||
-    !array_key_exists('hiwa-role', $_COOKIE)) {
+// Redirecting back to login page if the session is not set
+/$msg="";
+if (!isset($_SESSION['hiwa-user'])) || (!isset($_SESSION['hiwa-role'])) {
 	Header("Location: login.php");
 	exit();
 }
 
-$role=$_COOKIE['hiwa-role'];
+$role=$_SESSION['hiwa-role'];
 if ($role != 'admin') Header("Location: menu.php");
 ?>
 <html>
@@ -19,7 +18,7 @@ if ($role != 'admin') Header("Location: menu.php");
 
 <body>
 <?php require 'header.php';
-
+// functions will validate ips address and will not let other os commands to execute
 if (array_key_exists("ip", $_REQUEST)) {
 	if(filter_var($_REQUEST[ip], FILTER_VALIDATE_IP)){
 	echo "<P>pinging target IP address</P>";
